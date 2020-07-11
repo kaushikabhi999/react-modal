a42be832066c3965ce0c08e6aec217c07c6b3f90
salil0001 <salilsharma.sharma@gmail.com>
2020-07-11 14:23:24 +0530

    Created modal with a close button.

--- src/pages/commons/commons.js
+++ src/pages/commons/commons.js
@@ -1,12 +1,19 @@
 import React from "react";
-
+import Modal from "../../Modal/modal";
 export default function commons(props) {
+
   return (
     <>
       <nav>
         <a href="/">Sign In</a>
         <a href="/">Sign Up</a>
       </nav>
+      <Modal isOpen={false} closeButton={true}>
+        <form>
+          <input />
+          <input />
+        </form>
+      </Modal>
       {props.children}
     </>
   );
